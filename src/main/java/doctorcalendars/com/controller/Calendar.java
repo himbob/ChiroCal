db1b4b72b5dfed3190c80d1643d938af1995938a
Aaron Ruddick <aaron.ruddick@gmail.com>
2019-04-14 12:22:37 -0700

    New test

---
+++ src/main/java/doctorcalendars/com/controller/Calendar.java
@@ -0,0 +1,21 @@
+package doctorcalendars.com.controller;
+
+public
+class Calendar{
+
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar(String name) {
+        this.name = name;
+    }
+
+
+}
